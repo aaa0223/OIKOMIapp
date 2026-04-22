f0473f2b7607ae0cdb1284a0beda64236e493afd
aaa0223tt <akiyama0223taiki@gmail.com>
2026-04-22 20:56:04 +0900

    バージョンコードを8から9に更新

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.kobu.oikomi"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1.1"
     }
 
